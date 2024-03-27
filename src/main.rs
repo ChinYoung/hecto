b3a8e0b5b6bcdf929ee8f938ed6870049754af41
yang qin <73608+YANQIN1@users.noreply.git.i.mercedes-benz.com>
2024-03-27 09:16:26 +0800

    feat: 🎸 wip

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-use std::io::{self, stdout, Read};
-use termion::raw::IntoRawMode;
+mod editor;
 
-fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
-    for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
-        println!("{}", c);
-        if c == 'q' {
-            break;
-        }
-    }
-}
+fn main() {}
