92df1efbafe5c33dfdcf042b82a60254cce57e50
yang qin <woodichin@outlook.com>
2024-04-03 17:15:30 +0800

    feat: 🎸 wip

---
+++ src/document.rs
@@ -0,0 +1,13 @@
+use crate::Row;
+
+#[derive(Default)]
+pub struct Document {
+    rows: Vec<Row>,
+}
+impl Document {
+    pub fn open() -> Self {
+        let mut rows = Vec::new();
+        rows.push(Row::from("Hello hecto"));
+        Self { rows }
+    }
+}
