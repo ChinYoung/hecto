f963b9562cf72520b9f73a71b6711cb677df5a4d
yang qin <73608+YANQIN1@users.noreply.git.i.mercedes-benz.com>
2024-07-10 17:46:52 +0800

    fix: wip

--- src/document.rs
+++ src/document.rs
@@ -10,7 +10,10 @@ impl Document {
         rows.push(Row::from("Hello hecto"));
         Self { rows }
     }
-    pub fn row(&self,index: usize)-> Option<&Row> {
+    pub fn row(&self, index: usize) -> Option<&Row> {
         self.rows.get(index)
+    }
+    pub fn is_empty(&self) -> bool {
+        self.rows.is_empty()
     }
 }
