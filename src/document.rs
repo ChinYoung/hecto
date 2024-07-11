ca08107db4bf90e932d6bab452ecf63eb00ee316
yang qin <73608+YANQIN1@users.noreply.git.i.mercedes-benz.com>
2024-07-11 11:04:22 +0800

    feat: allow to open file

--- src/document.rs
+++ src/document.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 use crate::Row;
 
 #[derive(Default)]
@@ -5,10 +7,13 @@ pub struct Document {
     rows: Vec<Row>,
 }
 impl Document {
-    pub fn open() -> Self {
+    pub fn open(filename: &str) -> Result<Self, std::io::Error> {
+        let contents = fs::read_to_string(filename)?;
         let mut rows = Vec::new();
-        rows.push(Row::from("Hello hecto"));
-        Self { rows }
+        for value in contents.lines() {
+            rows.push(Row::from(value));
+        }
+        Ok(Self { rows })
     }
     pub fn row(&self, index: usize) -> Option<&Row> {
         self.rows.get(index)
