b92e3edd5034982b9477bfc54dd6bb4833db6bed
yang qin <73608+YANQIN1@users.noreply.git.i.mercedes-benz.com>
2024-07-17 17:21:42 +0800

    feat: allow to scroll

--- src/editor.rs
+++ src/editor.rs
@@ -1,3 +1,4 @@
+use crate::row;
 use crate::Document;
 use crate::Row;
 use crate::Terminal;
@@ -101,9 +102,12 @@ impl Editor {
     }
     fn move_cursor(&mut self, key: Key) {
         let Position { mut x, mut y } = self.cursor_position;
-        let size = self.terminal.size();
         let height = self.document.len();
-        let width = size.width.saturating_sub(1) as usize;
+        let width = if let Some(row) = self.document.row(y) {
+            row.len()
+        } else {
+            0
+        };
         match key {
             Key::Up => y = y.saturating_sub(1),
             Key::Down => {
